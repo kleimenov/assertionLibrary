4b8c9dde1b511cf77e253268a441b6120a4ae871
Иван <kleimenov416@gmail.com>
2020-09-21 16:55:47 -0400

    built couple of function and write assertions

--- assertFunction.js
+++ assertFunction.js
@@ -1,16 +1,23 @@
 
+//discover console.assert library
+
+//define function that return sum of two numbers
 const sumOfNumbers = function(x, y) {
     return x + y;
 }
 
+//define function that return sum of two numbers
 const multOfnumbers = function(x, y) {
     return x * y;
 }
 
+//define handful of variables
 let numberOne = 6;
 let numberTwo = 6;
+let randomValue = 6;
 
-console.assert(sumOfNumbers(numberOne, numberTwo) === 12);
-console.assert(sumOfNumbers(numberOne, numberTwo) ===8);
-console.assert(multOfnumbers(numberOne, numberTwo) === 23);
-console.assert(multOfnumbers(numberOne, numberTwo) ===45);
+//test our code
+console.assert(sumOfNumbers(numberOne, numberTwo) === sumOfNumbers(numberOne, numberTwo));
+console.assert(sumOfNumbers(numberOne, numberTwo) === sumOfNumbers(numberOne, numberTwo) - randomValue);
+console.assert(multOfnumbers(numberOne, numberTwo) === multOfnumbers(numberOne, numberTwo));
+console.assert(multOfnumbers(numberOne, numberTwo) === multOfnumbers(numberOne, numberTwo) + randomValue);
