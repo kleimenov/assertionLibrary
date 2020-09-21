42d717df3a2c013c40fdaea5b56c63a531262baf
Иван <kleimenov416@gmail.com>
2020-09-21 16:48:47 -0400

    built assertion function file

---
+++ assertFunction.js
@@ -0,0 +1,16 @@
+
+const sumOfNumbers = function(x, y) {
+    return x + y;
+}
+
+const multOfnumbers = function(x, y) {
+    return x * y;
+}
+
+let numberOne = 6;
+let numberTwo = 6;
+
+console.assert(sumOfNumbers(numberOne, numberTwo) === 12);
+console.assert(sumOfNumbers(numberOne, numberTwo) ===8);
+console.assert(multOfnumbers(numberOne, numberTwo) === 23);
+console.assert(multOfnumbers(numberOne, numberTwo) ===45);
