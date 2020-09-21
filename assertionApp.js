d0bb875026231924b6850f6ef63a1c33d9f46037
Иван <kleimenov416@gmail.com>
2020-09-21 19:56:05 -0400

    modified assertFunc add few features

--- assertionApp.js
+++ assertionApp.js
@@ -1,4 +1,4 @@
-//Write assertion function
+//Write assertion function assertFunc()
 
 const assertFunc = function(actualValue, expectedValue){
     return actualValue === expectedValue ? console.log("Good job") : console.log("Bad you");
@@ -8,3 +8,14 @@ assertFunc('cat', 'HHCDcat');
 assertFunc(20, 20);
 
 assertFunc('909', '909')
+
+//make refactoring assertFunc()
+
+const assertFunc = function(actualValue, expectedValue) {
+    const assertionPassed = "Assertion passed: " + actualValue + " === " + expectedValue;
+    const assertionFailed = "Assertion failed: " + actualValue + " !== " + expectedValue;
+    return actualValue === expectedValue ? console.log(assertionPassed) : console.log(assertionFailed);
+}
+
+assertFunc('909', '909')
+
