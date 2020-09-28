8e3ee6c169eeb5d7e0b2710d0cd6a84336868b39
Иван <kleimenov416@gmail.com>
2020-09-28 10:07:09 -0400

    refactored previous code removed assertFunc, add assertFunc as export from assertionApp.js file

--- lastElements.js
+++ lastElements.js
@@ -1,19 +1,14 @@
-const assertFunc = function(actualValue, expectedValue){
-    return actualValue === expectedValue ? console.log(`Assertion passed: ${actualValue}  === ${expectedValue}`) : console.log(`Assertion failed: ${actualValue}  !== ${expectedValue}`);
-}
+//refactored previous code removed assertFunc, add assertFunc as export from assertionApp.js file
+const assertFunc = require(`./assertionApp`)
 
 let array = [5, 4, 6];
-//let newArr;
+
 const lastElements = function(arr){
-    //newArr = array.slice(1);
-   // return newArr;
    return array.slice(1);
 }
+
 
 assertFunc(lastElements(array), array);
 assertFunc(array.length, 3);
 
-console.log(friends);
-let friends = "jjjj";
-//console.log(friends)
 
