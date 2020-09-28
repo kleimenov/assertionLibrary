12323532aaa73b1fbdc364c7811280efeb36fbe2
Иван <kleimenov416@gmail.com>
2020-09-28 09:47:52 -0400

    export function from assertionApp.js file to firstElement.js using module.exports and test it, everything work well

--- firstElement.js
+++ firstElement.js
@@ -3,8 +3,10 @@ let array = [5, 4, 6];
 const main = function(arr) {
     return arr[0];
 }
-
+/*
 const assertFunc = function(actualValue, expectedValue){
     return actualValue === expectedValue ? console.log(`Assertion passed: ${actualValue}  === ${expectedValue}`) : console.log(`Assertion failed: ${actualValue}  !== ${expectedValue}`);
 }
-assertFunc(main(array), 3);
+*/
+const assertFunc = require(`./assertionApp`)
+assertFunc(main(array), 5);
